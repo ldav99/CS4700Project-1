e42b9291f0fbe7c841ad17700769c930cd917d90
ldav99 <davidson.139@wright.edu>
2024-10-11 12:26:17 -0400

    Added Main and ask user for query

--- Main.py
+++ Main.py
@@ -8,3 +8,10 @@ with open('ACTORS.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
     for row in spamreader:
         print(', '.join(row))
+
+
+def main():
+    userQuery = str(input('Enter your query here: '))
+    print(userQuery)
+
+main()
