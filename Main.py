9a0c261ba387cef129f8b175984d9c16e9432e2d
ldav99 <davidson.139@wright.edu>
2024-10-11 12:08:55 -0400

    Imported CSV and added test code to make sure it was working properlly

--- Main.py
+++ Main.py
@@ -1,1 +1,10 @@
-print('Hello World!')
+#Luke Davidson, Jinho Nam
+#CS4700
+#Project 1
+
+import csv
+
+with open('ACTORS.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in spamreader:
+        print(', '.join(row))
