08bf603d6ff6818fe0d1025a92b2669e09d96635
ldav99 <davidson.139@wright.edu>
2024-10-12 14:11:59 -0400

    Added output file, more will be needed on it later.

--- Main.py
+++ Main.py
@@ -14,4 +14,16 @@ def main():
     userQuery = str(input('Enter your query here: '))
     print(userQuery)
 
+
+
+
+
+#Results need to be stored in output file
+#This will be neede towards the end
+    outputFile = open('RAoutput.csv', 'w')
+    outputFile.write(f'{userQuery}\n')
+    #outputFile.write(f'{queryResult}\n\n')
+
+    outputFile.close()
+
 main()
